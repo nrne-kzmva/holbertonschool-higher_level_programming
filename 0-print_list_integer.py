9d317fce627da9d15688f41b98a677f513d40077
nrne-kzmva <nrne.kzmva2003@gmail.com>
2025-05-03 21:06:36 +0000

    Add 0-print_list_integer.py and test file

--- 0-print_list_integer.py
+++ 0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        if not isinstance(i, int):
-            raise TypeError("list must contain only integers")
-        print("{:d}".format(i))
+    for number in my_list:
+        print("{:d}".format(number))
