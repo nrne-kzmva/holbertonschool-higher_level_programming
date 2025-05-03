62627cd445603abe240bc98aaccc6f15a095b713
nrne-kzmva <nrne.kzmva2003@gmail.com>
2025-05-03 21:46:43 +0000

    task3

---
+++ python-data_structures/2-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+my_list = [1, 2, 3]
+idx = 1
+new_element = 4
+new_list = replace_in_list(my_list, idx, new_element)
+print(new_list)
+print(my_list)
