1e9e669a4687b3cd7757c71a0fc5fe9152ba08d1
nrne-kzmva <nrne.kzmva2003@gmail.com>
2025-05-03 22:04:27 +0000

    task4

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
-#!/usr/bin/python3def print_reversed_list_integer(my_list=[]):
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
     for num in reversed(my_list):
         print("{}".format(num))
