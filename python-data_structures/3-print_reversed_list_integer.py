e54c4f3807a7e5b5625a05ed34703287f8ceb7d7
nrne-kzmva <nrne.kzmva2003@gmail.com>
2025-05-05 13:36:53 +0000

    my task

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for num in reversed(my_list):
-        print("{}".format(num))
+    """Prints all integers of a list in reverse order"""
+    if my_list:  # Check if the list is not empty
+        for i in reversed(my_list):  # Iterate through the list in reverse
+            print("{:d}".format(i))  # Print each integer as a decimal number
